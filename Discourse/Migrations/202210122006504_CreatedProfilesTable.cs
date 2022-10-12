332515556f18aa730beb82f2c986f25c2a7a6e39
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-10-12 15:07:15 -0500

    created profiles table

---
+++ Discourse/Migrations/202210122006504_CreatedProfilesTable.cs
@@ -0,0 +1,30 @@
+namespace Discourse.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreatedProfilesTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Profiles",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.String(nullable: false),
+                        Status = c.String(),
+                        Bio = c.String(),
+                        ProfilePicUrl = c.String(),
+                        BannerPicUrl = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Profiles");
+        }
+    }
+}
