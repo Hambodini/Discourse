1ef6c7181d0a4650e0b75323952b5b3b2b9aee55
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-10-12 13:19:08 -0500

    fixed reg error

--- Discourse/Controllers/AccountController.cs
+++ Discourse/Controllers/AccountController.cs
@@ -151,7 +151,13 @@ namespace Discourse.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { 
+                    UserName = model.Email, 
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    BirthDate = model.BirthDate,
+                };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
