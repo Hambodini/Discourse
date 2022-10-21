ef347c1692556210a2ebfe935b20a887c122c1b8
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-10-21 00:49:30 -0500

    got user posts displaying on profile page with most recent posts at the top

--- Discourse/Controllers/ProfileController.cs
+++ Discourse/Controllers/ProfileController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Web.Mvc;
 using Discourse.Models;
 using Microsoft.AspNet.Identity;
@@ -18,12 +19,23 @@ namespace Discourse.Controllers
         [Authorize]
         public ActionResult Profile()
         {
+            var post = new Post();
             var id = User.Identity.GetUserId();
+            post.UserId = id;
+
+            //Get posts of logged in user
+            var userPosts = _context.Posts.Where(p => p.UserId == id).OrderByDescending(p => p.TimeStamp).ToList();
+
             var pvm = new ProfileViewModel();
             pvm.User = _context.Users.Find(id);
             pvm.UserProfile = _context.Profiles.Where(p => p.UserId == id).ToArray()[0];
+            pvm.UserPosts = userPosts;
+            pvm.NewPost = post;
             return View(pvm);
         }
+
+        // POST: Profile
+
 
         [Authorize]
         public ActionResult Settings()
@@ -33,6 +45,29 @@ namespace Discourse.Controllers
             pvm.User = _context.Users.Find(id);
             pvm.UserProfile = _context.Profiles.Where(p => p.UserId == id).ToArray()[0];
             return View(pvm);
+        }
+
+        public ActionResult NewPost(ProfileViewModel model)
+        {
+            var timeStamp = DateTime.Now;
+
+            // Enter new post in db
+            var post = new Post();
+            post.UserId = model.User.Id;
+            post.Title = model.NewPost.Title;
+            post.Body = model.NewPost.Body;
+            post.TimeStamp = timeStamp;
+
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+
+            var pvm = model;
+
+            // Refresh posts in model
+            var userPosts = _context.Posts.Where(p => p.UserId == model.User.Id).OrderByDescending(p => p.TimeStamp).ToList();
+            pvm.UserPosts = userPosts;
+
+            return View("Profile", pvm);
         }
     }
 }
--- Discourse/Models/ProfileViewModel.cs
+++ Discourse/Models/ProfileViewModel.cs
@@ -1,8 +1,12 @@
-﻿namespace Discourse.Models
+﻿using System.Collections.Generic;
+
+namespace Discourse.Models
 {
     public class ProfileViewModel
     {
         public ApplicationUser User { get; set; }
         public Profile UserProfile { get; set; }
+        public Post NewPost { get; set; }
+        public List<Post> UserPosts { get; set; }
     }
 }
