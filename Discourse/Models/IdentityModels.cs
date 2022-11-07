501fdd69fe834913c63e72c0db59f63d3bd5790a
Hambodini <trimeyers1@gmail.com>
2022-11-07 13:13:54 -0600

    add natification table

--- Discourse/Models/IdentityModels.cs
+++ Discourse/Models/IdentityModels.cs
@@ -38,6 +38,7 @@ namespace Discourse.Models
         public DbSet<Profile> Profiles { get; set; }
         public DbSet<LikedPost> LikedPosts { get; set; }
         public DbSet<Friend> Friends { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
