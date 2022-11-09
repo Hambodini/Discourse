4c4200c5310169a26e418003e0dfdb8a45cfb960
Hambodini <trimeyers1@gmail.com>
2022-11-09 14:40:40 -0600

    updated hardcoded things

--- Discourse/Migrations/202211040657028_HardcodedPostsComments.cs
+++ Discourse/Migrations/202211040657028_HardcodedPostsComments.cs
@@ -17,12 +17,16 @@ INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [Fi
 INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (9, 9, N'9d2cc936-18c2-41ac-829e-cd7948b30cb6', N'I had some swag but some boomers told me to toss it in a volcano to save the world or something. kinda lame.', N'2022-11-04 01:31:13', N'Bilbo', N'TBaggins')
 INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (10, 10, N'52f892e9-8692-49f2-9c68-585741c10dd3', N'BEEEEFFFFFF', N'2022-11-04 01:33:21', N'Master', N'Beef')
 INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (11, 11, N'52f892e9-8692-49f2-9c68-585741c10dd3', N'goes back to college: Sir, Finishing This Fight.', N'2022-11-04 01:36:02', N'Master', N'Beef')
-INSERT INTO[dbo].[Comments]([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES(12, 12, N'52f892e9-8692-49f2-9c68-585741c10dd3', N'Sleeps in class: Wake Me When You Need Me.', N'2022-11-04 01:37:15', N'Master', N'Beef')
-INSERT INTO[dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES(13, 13, N'febc5f91-9747-4a3c-b38d-606484008719', N'1000 times and it is still good', N'2022-11-04 01:38:49', N'Techno', N'Logic')
-INSERT INTO[dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES(14, 14, N'febc5f91-9747-4a3c-b38d-606484008719', N'1000 times and its still good', N'2022-11-04 01:40:14', N'Techno', N'Logic')
-INSERT INTO[dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES(15, 15, N'febc5f91-9747-4a3c-b38d-606484008719', N'I love that movie', N'2022-11-04 01:41:36', N'Techno', N'Logic')
-INSERT INTO[dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES(16, 16, N'febc5f91-9747-4a3c-b38d-606484008719', N'I find this very insulting', N'2022-11-04 01:42:49', N'Techno', N'Logic')
-SET IDENTITY_INSERT[dbo].[Comments] OFF
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (12, 12, N'52f892e9-8692-49f2-9c68-585741c10dd3', N'Sleeps in class: Wake Me When You Need Me.', N'2022-11-04 01:37:15', N'Master', N'Beef')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (13, 13, N'febc5f91-9747-4a3c-b38d-606484008719', N'1000 times and it is still good', N'2022-11-04 01:38:49', N'Techno', N'Logic')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (14, 14, N'febc5f91-9747-4a3c-b38d-606484008719', N'1000 times and its still good', N'2022-11-04 01:40:14', N'Techno', N'Logic')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (15, 15, N'febc5f91-9747-4a3c-b38d-606484008719', N'I love that movie', N'2022-11-04 01:41:36', N'Techno', N'Logic')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (16, 16, N'febc5f91-9747-4a3c-b38d-606484008719', N'I find this very insulting', N'2022-11-04 01:42:49', N'Techno', N'Logic')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (17, 17, N'9d2cc936-18c2-41ac-829e-cd7948b30cb6', N'the author was J. R. R. Tolkien', N'2022-11-09 13:44:26', N'Bilbo', N'TBaggins')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (18, 18, N'9d2cc936-18c2-41ac-829e-cd7948b30cb6', N'It was very easy to pour my malice, cruelty and will to dominate all life into 4/5 stars', N'2022-11-09 13:46:10', N'Bilbo', N'TBaggins')
+INSERT INTO [dbo].[Comments] ([Id], [PostId], [UserId], [Body], [TimeStamp], [FirstName], [LastName]) VALUES (19, 19, N'52f892e9-8692-49f2-9c68-585741c10dd3', N'BBEEEEEEEEEEFFFFFFFFFFFF', N'2022-11-09 13:48:26', N'Master', N'Beef')
+SET IDENTITY_INSERT [dbo].[Comments] OFF
+
 
 ");
         }
--- Discourse/Migrations/202211091922360_HardcodedProfiles.cs
+++ Discourse/Migrations/202211091922360_HardcodedProfiles.cs
@@ -9,11 +9,12 @@ namespace Discourse.Migrations
         {
             Sql(@"SET IDENTITY_INSERT [dbo].[Profiles] ON
 INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (1, N'66b84ac3-2209-4c0f-a485-bd961c25e679', NULL, NULL, N'/wwwroot/ProfilePic/NoPic.jpg', N'/wwwroot/BannerPic/NoBanner.jpg')
-INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (2, N'091b17e2-4dd9-4d0f-8aa0-f6f1cf4f8312', NULL, NULL, N'/wwwroot/ProfilePic/NoPic.jpg', N'/wwwroot/BannerPic/NoBanner.jpg')
-INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (3, N'9d2cc936-18c2-41ac-829e-cd7948b30cb6', NULL, NULL, N'/wwwroot/ProfilePic/NoPic.jpg', N'/wwwroot/BannerPic/NoBanner.jpg')
-INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (4, N'52f892e9-8692-49f2-9c68-585741c10dd3', NULL, NULL, N'/wwwroot/ProfilePic/NoPic.jpg', N'/wwwroot/BannerPic/NoBanner.jpg')
-INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (5, N'febc5f91-9747-4a3c-b38d-606484008719', NULL, NULL, N'/wwwroot/ProfilePic/NoPic.jpg', N'/wwwroot/BannerPic/NoBanner.jpg')
+INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (10, N'091b17e2-4dd9-4d0f-8aa0-f6f1cf4f8312', NULL, N'1 Bilion dollars!!!', N'/wwwroot/ProfilePic/091b17e2-4dd9-4d0f-8aa0-f6f1cf4f8312.jpg', N'/wwwroot/BannerPic/091b17e2-4dd9-4d0f-8aa0-f6f1cf4f8312.jpg')
+INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (14, N'9d2cc936-18c2-41ac-829e-cd7948b30cb6', NULL, N'1 ring 2 rule them all.', N'/wwwroot/ProfilePic/9d2cc936-18c2-41ac-829e-cd7948b30cb6.jpg', N'/wwwroot/BannerPic/9d2cc936-18c2-41ac-829e-cd7948b30cb6.jpg')
+INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (16, N'52f892e9-8692-49f2-9c68-585741c10dd3', NULL, N'vegan, non-organic, whole food hero', N'/wwwroot/ProfilePic/52f892e9-8692-49f2-9c68-585741c10dd3.jpg', N'/wwwroot/BannerPic/52f892e9-8692-49f2-9c68-585741c10dd3.jpg')
+INSERT INTO [dbo].[Profiles] ([Id], [UserId], [Status], [Bio], [ProfilePicUrl], [BannerPicUrl]) VALUES (17, N'febc5f91-9747-4a3c-b38d-606484008719', NULL, N'Buy it, use it, break it, fix it, trash it, change it, mail, upgrade it, Charge it, point it, zoom it, press it, snap it, work it, quick erase it, Write it, cut it, paste it, save it, load it, check it, quick rewrite it, Plug it, play it, burn it, rip it, rip it', N'/wwwroot/ProfilePic/febc5f91-9747-4a3c-b38d-606484008719.jpg', N'/wwwroot/BannerPic/febc5f91-9747-4a3c-b38d-606484008719.jpg')
 SET IDENTITY_INSERT [dbo].[Profiles] OFF
+
 ");
         }
         
