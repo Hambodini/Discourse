fa748578e374081ad91dd87b3952ee1f1223b9ad
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-11-21 14:43:51 -0600

    got a part of the reporting post feature working

--- Discourse/Controllers/AdminController.cs
+++ Discourse/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Discourse.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,45 @@ namespace Discourse.Controllers
 {
     public class AdminController : Controller
     {
-        
+        private ApplicationDbContext _context;
+
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             //Display all reports
+            NotImplementedException ex = new NotImplementedException();
+            throw ex;
+        }
+
+        [HttpGet]
+        public ActionResult AddReportedPost(int postId)
+        {
+            var reporterId = User.Identity.GetUserId();
+            var dateReported = DateTime.Now;
+            var post = _context.Posts.Find(postId);
+
+            var reportedPost = new ReportedPost();
+            reportedPost.ReporterUserId = reporterId;
+            reportedPost.DateReported = dateReported;
+            reportedPost.ReportedUserId = post.UserId;
+            reportedPost.PostId = post.Id;
+            reportedPost.FirstName = post.FirstName;
+            reportedPost.LastName = post.LastName;
+            reportedPost.Title = post.Title;
+            reportedPost.Body = post.Body;
+            reportedPost.TimeStamp = post.TimeStamp;
+
+            return View(reportedPost);
+        }
+
+        [HttpPost]
+        public ActionResult AddReportedPostConfirmed(ReportedPost reportedPost)
+        {
+            int i = 0;
             NotImplementedException ex = new NotImplementedException();
             throw ex;
         }
